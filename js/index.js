91f1303a7e815a5a820254ca50c84f7376fae9bd
+ <+>
2023-06-27 11:06:53 +0300

    fetched username

--- js/index.js
+++ js/index.js
@@ -6,6 +6,16 @@ const form=document.getElementById('github-form');
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     let userName = e.target.search.value;
+
+    fetch(`https://api.github.com/search/users?q=${userName}`,{
+      headers:{
+         "Accept": "application/vnd.github.v3+json "
+      }
+    })
+    .then(res=>res.json())
+    .then(data => {
+      userList.innerHTML = ''; //clear previous data of the user
+
   });
 
 
