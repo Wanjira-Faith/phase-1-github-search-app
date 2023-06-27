5f1c5539b8a182452c3c2e3437983349540de7ac
+ <+>
2023-06-27 11:02:18 +0300

    declared userList and repoList

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',() => {
+const userList=document.getElementById('user-list');
+const repoList=document.getElementById('repos-list');
+
+const form=document.getElementById('github-form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let userName = e.target.search.value;
+  });
+});    
